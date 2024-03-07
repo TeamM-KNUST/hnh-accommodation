80eb839f086f3987fb42f29ffcb56facd761a4d0
abudusamad <abudusamed@gmail.com>
2024-03-07 00:01:34 +0000

    okau

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -10,6 +10,7 @@ const font = Poppins({
 interface HeaderProps {
 	label: string;
 }
+
 export const Header = ({ label }: HeaderProps) => {
 	return (
 		<div className="w-full flex flex-col gap-y-4 items-center justify-center">

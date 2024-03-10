84259454b065c767b3e8525b95589bd53d9b843b
abudusamad <abudusamed@gmail.com>
2024-03-10 23:20:29 +0000

    making the auth for the credential provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,27 @@
+import bcrypt from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
 
-import type { NextAuthConfig } from "next-auth";
 import { getUserByEmail } from "@/data/user";
 import { LoginSchema } from "@/schemas";
+import type { NextAuthConfig } from "next-auth";
 
 export default {
 	providers: [
 		CredentialsProvider({
 			async authorize(credentials) {
 				const validatedFields = LoginSchema.safeParse(credentials);
-				if (!validatedFields.success) {
-					return null;
+				if (validatedFields.success) {
+					const { email, password } = validatedFields.data;
+					const user = await getUserByEmail(email);
+					if (!user || !user.password) {
+						return null;
+					}
+					const passwordMatch = await bcrypt.compare(password, user.password);
+					if (passwordMatch) {
+						return user;
+					}
 				}
-				const { email, password } = validatedFields.data;
-				const user = await getUserByEmail(email);
-				if (!user || !user.password) {
-					return null;
-				}
-				const passwordMatch = await bcrypt.compare(password, user.password);
-				if (!passwordMatch) {
-					return null;
-				}
-				return user
+				return null;
 			},
 		}),
 	],

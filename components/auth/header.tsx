cf2048b2af22278ba5944d4c037ab0193fad78b8
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-13 22:34:23 +0000

    Making the logo for the web page

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
+import { LogOut } from "lucide-react";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
 
 const font = Poppins({
 	subsets: ["latin"],
@@ -15,7 +17,13 @@ export const Header = ({ label }: HeaderProps) => {
 	return (
 		<div className="w-full flex flex-col gap-y-4 items-center justify-center">
 			<h1 className={cn("text-3xl font-semibold", font.className)}>
-				🇬🇭 Hostel Finder
+				<Image
+					fill
+					alt="Logo"
+				
+				src={logo.png}
+				/>
+				H&H Accomodation
 			</h1>
 			<p className="text-muted-foreground text-sm">{label}</p>
 		</div>

1ec1955a74267362237a30dfb7317246b6b9914d
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-16 23:10:04 +0000

    addig the data for the verication token

--- data/verification-token.ts
+++ data/verification-token.ts
@@ -1,2 +1,25 @@
 import { db } from "@/lib/db";
 
+export const getVericationTokenByToken = async (token: string) => {
+	try {
+		const verificationToken = await db.verificationToken.findUnique({
+			where: {
+				token,
+			},
+		});
+	} catch {
+		return null;
+	}
+};
+
+export const getVericationTokenByEmail = async (email: string) => {
+	try {
+		const verificationToken = await db.verificationToken.findFirst({
+			where: {
+				email,
+			},
+		});
+	} catch {
+		return null;
+	}
+};

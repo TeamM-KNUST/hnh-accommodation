67ee42fb050dc3ef48457e27a03afba4717f4b22
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-16 22:24:01 +0000

    Adding the logo to the page

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -1,5 +1,4 @@
 import { cn } from "@/lib/utils";
-import { LogOut } from "lucide-react";
 import { Poppins } from "next/font/google";
 import Image from "next/image";
 
@@ -7,7 +6,6 @@ const font = Poppins({
 	subsets: ["latin"],
 	weight: ["600"],
 });
-
 
 interface HeaderProps {
 	label: string;
@@ -17,7 +15,14 @@ export const Header = ({ label }: HeaderProps) => {
 	return (
 		<div className="w-full flex flex-col gap-y-4 items-center justify-center">
 			<h1 className={cn("text-3xl font-semibold", font.className)}>
-				
+				<div className="flex items-center justify-center">
+					<Image
+						src="/logo.png"
+						alt="H&H Accomodation"
+						width={38}
+						height={30}
+					/>
+				</div>
 				H&H Accomodation
 			</h1>
 			<p className="text-muted-foreground text-sm">{label}</p>

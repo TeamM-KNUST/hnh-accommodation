bc74d6efe7c9bea8a3db657687283978792a5703
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-22 21:08:02 +0000

    making the token verification when user signIn with credentials

--- components/auth/verification-form.tsx
+++ components/auth/verification-form.tsx
@@ -1,53 +1,49 @@
 "use client";
-import {BeatLoader} from "react-spinners";
+import { BeatLoader } from "react-spinners";
 
-import { CardWrapper } from "./card-wrapper";
+import { NewVerification } from "@/actions/new-verification";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useState } from "react";
-import { NewVerification } from "@/actions/new-verification";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
+import { CardWrapper } from "./card-wrapper";
 
 export const VerificationForm = () => {
-    const [error, setError] = useState<string | undefined>()
-    const [success, setSucess] =useState<string | undefined>()
-    
-    const searchParams = useSearchParams();
-    const token = searchParams.get("token");
+	const [error, setError] = useState<string | undefined>();
+	const [success, setSucess] = useState<string | undefined>();
 
-    const onSubmit = useCallback(() => {
-        if (success || error) return;
-        if (!token) {
-            setError("Missing token!")
-            return;
-        }
-        NewVerification(token)
-            .then((data) => {
-                setSucess(data.success)
-                setError(data.error)
-            })
-            .catch(() => {
-            setError("Something Went Wrong!")
-        })
-        
-    }, [token,success, error])
-    useEffect(() => {
-        onSubmit();
-    }, [onSubmit])
-    return (
+	const searchParams = useSearchParams();
+	const token = searchParams.get("token");
 
-        <CardWrapper
-            headerLabel="Confirm your email address"
-            backButtonHref="auth/login"
-            backButtonLabel="Back to login"
-        
-        >
-            <div className="flex w-full items-center justify-center">
-                <BeatLoader />
-                <FormError message={error} />
-                <FormSuccess message={success} />
-            </div>
-
-        </CardWrapper>
-    );
-}
+	const onSubmit = useCallback(() => {
+		if (success || error) return;
+		if (!token) {
+			setError("Missing token!");
+			return;
+		}
+		NewVerification(token)
+			.then((data) => {
+				setSucess(data.success);
+				setError(data.error);
+			})
+			.catch(() => {
+				setError("Something Went Wrong!");
+			});
+	}, [token, success, error]);
+	useEffect(() => {
+		onSubmit();
+	}, [onSubmit]);
+	return (
+		<CardWrapper
+			headerLabel="Confirm your email address"
+			backButtonHref="auth/login"
+			backButtonLabel="Back to login"
+		>
+			<div className="flex w-full items-center justify-center">
+				{!success && !error && <BeatLoader />}
+				<FormSuccess message={success} />
+				{!success && <FormError message={error} />}
+			</div>
+		</CardWrapper>
+	);
+};

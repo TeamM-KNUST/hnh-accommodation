5ffad50c6db64770be750c395960bf2fae0809fe
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-22 20:54:02 +0000

    making the token component for the page

--- components/auth/verification-form.tsx
+++ components/auth/verification-form.tsx
@@ -2,10 +2,40 @@
 import {BeatLoader} from "react-spinners";
 
 import { CardWrapper } from "./card-wrapper";
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { NewVerification } from "@/actions/new-verification";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
 
 export const VerificationForm = () => {
-    return (
+    const [error, setError] = useState<string | undefined>()
+    const [success, setSucess] =useState<string | undefined>()
+    
+    const searchParams = useSearchParams();
+    const token = searchParams.get("token");
+
+    const onSubmit = useCallback(() => {
+        if (success || error) return;
+        if (!token) {
+            setError("Missing token!")
+            return;
+        }
+        NewVerification(token)
+            .then((data) => {
+                setSucess(data.success)
+                setError(data.error)
+            })
+            .catch(() => {
+            setError("Something Went Wrong!")
+        })
         
+    }, [token,success, error])
+    useEffect(() => {
+        onSubmit();
+    }, [onSubmit])
+    return (
+
         <CardWrapper
             headerLabel="Confirm your email address"
             backButtonHref="auth/login"
@@ -14,6 +44,8 @@ export const VerificationForm = () => {
         >
             <div className="flex w-full items-center justify-center">
                 <BeatLoader />
+                <FormError message={error} />
+                <FormSuccess message={success} />
             </div>
 
         </CardWrapper>

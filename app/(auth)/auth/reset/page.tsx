325e376b2bd157a4612eda9c0e3f3c490335e0a4
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-25 01:24:57 +0000

    making the password reset form

---
+++ app/(auth)/auth/reset/page.tsx
@@ -0,0 +1,9 @@
+import { PassswordResetForm } from "@/components/auth/password-reset-form";
+
+const PasswordRestPage = () => {
+    return (
+        <PassswordResetForm />  
+     );
+}
+ 
+export default PasswordRestPage;
---
+++ components/auth/password-reset-form.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
+import { CardWrapper } from "./card-wrapper";
+
+import * as z from "zod";
+
+import { login } from "@/actions/login";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { LoginSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState, useTransition } from "react";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+
+export const PassswordResetForm = () => {
+	const [pending, startTransition] = useTransition();
+	const [error, setError] = useState<string | undefined>("");
+	const [success, setSuccess] = useState<string | undefined>("");
+
+	const form = useForm<z.infer<typeof LoginSchema>>({
+		resolver: zodResolver(LoginSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+		setError("");
+		setSuccess("");
+
+		startTransition(() => {
+			login(values)
+				.then((data) => {
+					if (data?.error) {
+						setError(data.error);
+						form.reset();
+					}
+					if (data?.success) {
+						setSuccess(data.success);
+					}
+				})
+				.catch((error) => {
+					setError("Something went wrong");
+					form.reset();
+				});
+		});
+	};
+	return (
+		<CardWrapper
+			headerLabel="Reset your password"
+			backButtonLabel="Back to login"
+			backButtonHref="/auth/login"
+		>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										{...field}
+										placeholder="john.doe@example.com"
+										type="email"
+										disabled={pending}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormError message={error} />
+					<FormSuccess message={success} />
+					<Button type="submit" className="w-full" disabled={pending}>
+						Reset Password
+					</Button>
+				</form>
+			</Form>
+		</CardWrapper>
+	);
+};

d4963fd1b0f058a27b7eacc3b801a4c321178b5f
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-04-26 03:09:10 +0000

    making the form error and form sucess dynamic for different application

--- components/form-error.tsx
+++ components/form-error.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 interface FormErrorProps {
@@ -5,9 +8,23 @@ interface FormErrorProps {
 }
 
 export const FormError = ({ message }: FormErrorProps) => {
-	if(!message) return null;
+	const [visible, setVisible] = useState(!!message);
+
+	useEffect(() => {
+		if (message) {
+			setVisible(true);
+			const timer = setTimeout(() => {
+				setVisible(false);
+			}, 5000); // 5000ms = 5s
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, [message]);
+	if(!visible) return null;
 	return (
-		<div className="bg-rose-500/15 p-3 rounded-md flex items-center gap-x-2  text-sm text-rose-500">
+		<div className="bg-rose-500/15 p-3 rounded-md flex items-center gap-x-2  text-sm text-rose-500 border-l-8 border-rose-500">
 			<FaExclamationTriangle className="w-4 h-4" />
 			<p>{message}</p>
 		</div>
--- components/form-success.tsx
+++ components/form-success.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { CheckCircleIcon } from "lucide-react";
 
 interface FormSuccessProps {
@@ -5,9 +6,24 @@ interface FormSuccessProps {
 }
 
 export const FormSuccess = ({ message }: FormSuccessProps) => {
-    if (!message) return null;
+	const [visible, setVisible] = useState(!!message);
+
+	useEffect(() => {
+		if (message) {
+			setVisible(true);
+			const timer = setTimeout(() => {
+				setVisible(false);
+			}, 5000); // 5000ms = 5s
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, [message]);
+
+	if (!visible) return null;
 	return (
-		<div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2  text-sm text-emerald-500">
+		<div className="bg-emerald-500/15 p-3 rounded-md flex items-center gap-x-2  text-sm text-emerald-500 border-l-8 border-emerald-500">
 			<CheckCircleIcon className="w-4 h-4" />
 			<p>{message}</p>
 		</div>

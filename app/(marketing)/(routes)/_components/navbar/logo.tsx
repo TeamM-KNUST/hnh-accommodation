d1c96521acedcf92c5e96c04649592be07ac04ea
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-21 13:52:51 +0000

    Adding logo: 2

--- app/(marketing)/(routes)/_components/navbar/logo.tsx
+++ app/(marketing)/(routes)/_components/navbar/logo.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import Image from "next/image"
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export const Logo = () => {
-    const router = useRouter();
-    return (
-        <div>
-            <Image
-                
-                src="/logo.png"
-                alt="logo"
-                width={50}
-                height={50}
-                className="hidden md:block cursor-pointer"
-                onClick={() => router.push("/")}
-            />
-            
-        </div>
-    )
-}
+	const router = useRouter();
+	return (
+		<div className="flex items-center justify-center  gap-2  ">
+			<Image
+				src="/logo.png"
+				alt="logo"
+				width={50}
+				height={50}
+				className="hidden md:block cursor-pointer"
+				onClick={() => router.push("/")}
+			/>
+			<h2 className="text-3xl text-blue-700 hidden md:block">Accomodation</h2>
+		</div>
+	);
+};

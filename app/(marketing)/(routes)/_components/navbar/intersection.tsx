cd38c155e65caeda550f33e2f73a039805ee4eed
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-22 14:25:59 +0000

    Adding constant of picturesclear

--- app/(marketing)/(routes)/_components/navbar/intersection.tsx
+++ app/(marketing)/(routes)/_components/navbar/intersection.tsx
@@ -1,15 +1,68 @@
-import { Button } from "@/components/ui/button";
+"use client";
+import Image from "next/image";
+import "../style/intersection.css";
 
-import "../style/intersection.css"
+import { cn } from "@/lib/utils";
+import { Inter, Poppins } from "next/font/google";
+import { useEffect, useRef } from "react";
+
+const font = Poppins({
+	subsets: ["latin"],
+	weight: ["600", "700", "800"],
+});
+
+const inter = Inter({
+	subsets: ["latin"],
+	weight: ["800"],
+});
 
 export const InterSection = () => {
-    return (
-        <div className="section-wrapper">
-            <div className="relatve w-full flex items-center h-[40.625rem] justify-center overflow-x-hidden break-out">
+	const imageRef = useRef<HTMLImageElement>(null);
+	useEffect(() => {
+		const currentImageRef = imageRef.current;
+		if (currentImageRef) {
+			imageRef.current.style.animation = " rotate 10s linear";
+		}
+		return () => {
+			if (currentImageRef) {
+				currentImageRef.style.animation = "";
+			}
+		};
+	}, []);
 
-            </div>
-        
-
-        </div>
-    );
-}
+	return (
+		<div className="section-wrapper">
+			<div className="relatve w-full flex items-center h-[40.625rem] justify-center overflow-x-hidden break-out">
+				<Image
+					ref={imageRef}
+					src={
+						"https://a.hwstatic.com/image/upload/f_auto/v1645010222/pwa/whosgoing/los-patios-card_png.jpg"
+					}
+					alt="Connect with travellers"
+					width={289}
+					height={328}
+					loading="lazy"
+				/>
+			</div>
+			<div className="flex flex-col items-center mt:[-3.125rem] text-center px-4 text-white">
+				<h2
+					className={cn(
+						"sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl",
+						font.className
+					)}
+				>
+					See <span className="text-[#f6a90e]">who&apos;s</span> going to hostel
+				</h2>
+				<p
+					className={cn(
+						"my-2 sm:text-2xl md:text-3xl lg:text-4xl xl:text-xl",
+						inter.className
+					)}
+				>
+					Connect with other travellers staying in the same hostel or city as
+					you.
+				</p>
+			</div>
+		</div>
+	);
+};
---
+++ data/constant.ts
@@ -0,0 +1,49 @@
+export const FLAGS_IMAGE_URLS = {
+	brazil:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/brazil_png.png",
+	china:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/china_png.png",
+	france:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/france_png.png",
+	germany:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/germany_png.png",
+	ireland:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/ireland_png.png",
+	italy:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/italy_png.png",
+	poland:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/poland_png.png",
+	portugal:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/portugal_png.png",
+	southKorea:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/south-korea_png.png",
+	spain:
+		"https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/spain_png.png",
+	usa: "https://a.hwstatic.com/image/upload/q_auto,f_auto,h_24/v1644506092/pwa/whosgoing/usa_png.png",
+};
+
+export const AVATAR_IMAGE_URLS = {
+	allesandra:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-alessandra.jpg",
+	mike: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/miser-mike.jpg",
+	anna: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-anna.jpg",
+	eve: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-eve.jpg",
+	luiza:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-f-luiza.jpg",
+	jenny:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-jenny.jpg",
+	laura:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-laura.jpg",
+	luka: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-luka.jpg",
+	nathan:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-nathan.jpg",
+	pan: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-pan.jpg",
+	paula:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-paula.jpg",
+	pierre:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-pierre.jpg",
+	tom: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-tom.jpg",
+	victoria:
+		"https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-victoria.jpg",
+	lin: "https://a.hwstatic.com/image/upload/f_auto,q_75,h_166/v1644506092/pwa/whosgoing/user-lin.jpg",
+};

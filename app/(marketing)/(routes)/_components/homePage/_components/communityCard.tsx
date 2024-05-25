0bab2d9f08a8b2d52857806b87492e5facad5d13
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-25 15:16:10 +0000

    Adding social solo

--- app/(marketing)/(routes)/_components/homePage/_components/communityCard.tsx
+++ app/(marketing)/(routes)/_components/homePage/_components/communityCard.tsx
@@ -3,8 +3,6 @@ import { ArrowRight, Clock5 } from "lucide-react";
 import Image from "next/image";
 import { FaVenus } from "react-icons/fa";
 import { HomePageAvatar } from "./homepageavatar";
-import "../../style/community.css"
-
 interface CommunityCardsProps {
 	frameUrl: string;
 	participantsAvatarUrls: string[];
@@ -30,36 +28,51 @@ export const CommunityCards = ({
 }: CommunityCardsProps) => {
 	const isVideoUrl = /(mp4)$/m.test(frameUrl);
 	return (
-		<div className="card-wrapper">
+		<div className="relative md:h-[34.25rem] md:w-[18.1875rem] h-[19.75rem] w-[11.5rem]">
 			{isVideoUrl ? (
-				<video autoPlay loop muted playsInline title="" className="media">
+				<video
+					autoPlay
+					loop
+					muted
+					playsInline
+					title=""
+					className="absolute w-full h-full rounded-lg object-cover"
+				>
 					<source src={frameUrl} type="video/mp4" />
 				</video>
 			) : (
-				<Image src={frameUrl} alt="" className="media" width={32} height={32} />
+				<Image
+					src={frameUrl}
+					alt=""
+					className="absolute w-full h-full rounded-lg object-cover"
+					width={32}
+					height={32}
+				/>
 			)}
-			<div className="content">
-				<div className="content-topGroup">
-					<div className="content-avatar">
-						<HomePageAvatar imgUrl={avatarUrl} size={32} alt="" />
+			<div className="relative w-full h-full flex flex-col justify-between items-center text-white rounded-md px-3 py-2 z-20">
+				<div className="flex items-center top-4 left-4 ">
+					<div className="relative mr-2">
+						<HomePageAvatar imgUrl={avatarUrl} size={50} alt=""  />
 					</div>
-					<div className="content-name">{name}</div>
+					<div className="font-semibold text-xs translate-y-[-6px]">{name}</div>
 				</div>
-				<div className="content-bottomGroup">
-					<h4 className="content-title">{title}</h4>
-					<div className="content-info">
-						<span className="content-time">
-							<Clock5 size={16} />
+				<div className="flex flex-col">
+					<h4 className="font-normal mb-2 text-xl tracking-[-0.1px] ">
+						{title}
+					</h4>
+					<div className="flex font-semibold mb-1 leading-3">
+						<span className="flex items-center">
+							<Clock5 className="h-[0.624rem] w-[0.625rem] mr-1 translate-y-[-3px]" />
 							{time}
 						</span>
-						<span className="content-location">
-							<FaVenus size={16} />
+						<span className="flex items-center">
+							<FaVenus className="h-[0.624rem] w-[0.625rem] mr-1 translate-y-[-3px]" />
 							{location}
 						</span>
 					</div>
-					<div className="content-bottom ">
-						<div className="content-peopleCount">
-							<div className="content-avatarsGroup">
+					<div className="flex justify-between">
+						<div className="flex items-center font-semibold text-sm ">
+							<div className="relative w-16 translate-y-[-5px]">
 								{/* {participantsAvatarUrls.map((avatarUrl, index) => (
 									<HomePageAvatar
 										key={index}
@@ -69,11 +82,12 @@ export const CommunityCards = ({
 									/>
 								))} */}
 							</div>
-							<div>{`+${participantsCount} coming`}</div>
+							<div className="self-end">{`+${participantsCount} coming`}</div>
 							<Button
-								variant="default"
-							
-								className="content-button w-full ml-6">
+								variant="purple"
+								className="flex items-center font-semibold text-sm justify-center  tracking-[-0.01875rem]  ml-6"
+								size="sm"
+							>
 								Join
 								<ArrowRight size={16} className="text-white" />
 							</Button>

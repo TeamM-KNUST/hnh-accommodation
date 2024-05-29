bf293d9a4c860830896145799e59a5af0b50ba25
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-05-29 12:32:00 +0000

    hero section

--- app/(marketing)/(routes)/page.tsx
+++ app/(marketing)/(routes)/page.tsx
@@ -6,20 +6,19 @@ import { Hero } from "./_components/homePage/hero";
 import { Sayhello } from "./_components/homePage/sayhello";
 import { SoloToSocial } from "./_components/homePage/SoloToSocial/solotosocial";
 const font = Poppins({
-	subsets: ["latin"],
-	weight: ["600"],
+  subsets: ["latin"],
+  weight: ["600"],
 });
 
 export default function Home() {
-	return (
-		<Container>
-			<div className="flex flex-col gap-y-6">
-				<Hero />
-				<Connect />
-				<InterSection />
-				<Sayhello />
-				<SoloToSocial />
-			</div>
-		</Container>
-	);
+  return (
+    <Container>
+      <div className="flex flex-col gap-y-6">
+        <Connect />
+        <InterSection />
+        <Sayhello />
+        <SoloToSocial />
+      </div>
+    </Container>
+  );
 }

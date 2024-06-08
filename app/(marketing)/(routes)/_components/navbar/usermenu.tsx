68b7875a3c90a1a55c86aab76e0f118ec9957e4e
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-08 09:27:02 +0000

    Adding the sections

--- app/(marketing)/(routes)/_components/navbar/usermenu.tsx
+++ app/(marketing)/(routes)/_components/navbar/usermenu.tsx
@@ -12,6 +12,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { AvatarImg } from "./avatarImage";
 import { MenuIcon } from "lucide-react";
+import { AddHostelModal } from "@/app/modals/add-hostel";
 
 export const UserMenu = () => {
   const router = useRouter();
@@ -35,7 +36,15 @@ export const UserMenu = () => {
         cursor-pointer
         "
         >
-          Hostel your home
+            
+          <AddHostelModal
+            trigger={
+              <div className="flex items-center gap-3">
+                <span>Add Hostel</span>
+              </div>
+            }
+          />
+         
         </div>
 
         <div

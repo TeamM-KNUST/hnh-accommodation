7cdf892391a6a7ddc3ba30807cbc8d14d2921483
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-09 19:21:14 +0000

    Fixed popover error

--- components/Popover/user-popover.tsx
+++ components/Popover/user-popover.tsx
@@ -1,32 +1,35 @@
 "use client";
 
-import { usePopoverModal } from "@/hooks/use-popover-modal";
-import { PopoverTrigger } from "@radix-ui/react-popover";
-import { Popover, PopoverContent } from "../ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { usePopoverModal } from "@/hooks/usepopover";
 
 interface UserPopoverProps {
-	children: React.ReactNode;
-	trigger: React.ReactNode;
-	onClick?: () => void;
+  children: React.ReactNode;
+  trigger: React.ReactNode;
+  onClick?: () => void;
 }
 
-export const UserPopover = ({ children, trigger , onClick}: UserPopoverProps) => {
-	const { isOpen, onClose, onOpen } = usePopoverModal();
+export const UserPopover = ({
+  children,
+  trigger,
+  onClick,
+}: UserPopoverProps) => {
+  const { isOpen, onClose, onOpen } = usePopoverModal();
 
-	const handleClick = () => {
-		if (isOpen) {
-			onOpen();
-		} else {
-			onClose();
-		}
-	};
+  const handleClick = () => {
+    if (isOpen) {
+      onOpen();
+    } else {
+      onClose();
+    }
+  };
 
-	return (
-		<Popover open={isOpen} onOpenChange={onClose} >
-			<PopoverTrigger asChild onClick={onClick}>
-				<div onClick={handleClick}>{trigger}</div>
-			</PopoverTrigger>
-			<PopoverContent >{children}</PopoverContent>
-		</Popover>
-	);
+  return (
+    <Popover open={isOpen} onOpenChange={onClose}>
+      <PopoverTrigger asChild onClick={onClick}>
+        <div onClick={handleClick}>{trigger}</div>
+      </PopoverTrigger>
+      <PopoverContent>{children}</PopoverContent>
+    </Popover>
+  );
 };

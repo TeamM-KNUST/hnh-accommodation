efadc1cce54c16a892853f528b49f03982a19a0d
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-06-09 19:36:08 +0000

    Fixed popover modal

--- components/Popover/user-popover.tsx
+++ components/Popover/user-popover.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { usePopoverModal } from "@/hooks/usepopover";
 
 interface UserPopoverProps {
   children: React.ReactNode;
@@ -14,20 +13,12 @@ export const UserPopover = ({
   trigger,
   onClick,
 }: UserPopoverProps) => {
-  const { isOpen, onClose, onOpen } = usePopoverModal();
 
-  const handleClick = () => {
-    if (isOpen) {
-      onOpen();
-    } else {
-      onClose();
-    }
-  };
 
   return (
-    <Popover open={isOpen} onOpenChange={onClose}>
+    <Popover >
       <PopoverTrigger asChild onClick={onClick}>
-        <div onClick={handleClick}>{trigger}</div>
+        <div>{trigger}</div>
       </PopoverTrigger>
       <PopoverContent>{children}</PopoverContent>
     </Popover>

9aac1616eb787ed925431a49a5fc493b14db63aa
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-01 12:15:40 +0000

    Making the litingId page

---
+++ app/(dashboard)/dashboard/listings/[listingId]/_components/lisiting-info.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+
+import { AvatarImg } from "@/app/(marketing)/(routes)/_components/navbar/avatarImage";
+import { useCountries } from "@/hooks/useCountries";
+import { User } from "@prisma/client";
+import dynamic from "next/dynamic";
+import Sleep from "./sleep";
+import Offers from "./offer";
+
+interface ListingInfoProps {
+  user: User;
+  description: string;
+  locationValue: string | null;
+}
+
+const Map = dynamic(() => import("@/components/map"), {
+  ssr: false,
+});
+
+export const ListingInfo = ({
+  user,
+  description,
+  locationValue,
+}: ListingInfoProps) => {
+
+  return (
+    <div className="col-span-4 flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex-row items-center gap-2">
+          <div>Hosted by {user.name}</div>
+          <AvatarImg src={user?.image} />
+        </div>
+        <hr />
+        <div className="flex flex-col">
+          <p className="text-4xl font-bold text-[#FF5A5F]">
+            Air<span className="text-black">cover</span>
+          </p>
+          <p className="text-neutral-500 pt-3">
+            Every booking includes free protection from Host cancellations,
+            listing inaccuracies, and other issues like trouble checking in.
+          </p>
+          <p className="text-black font-bold underline pt-3 cursor-pointer">
+            Learn more
+          </p>
+        </div>
+        <hr />
+        <p className="text-lg font-light text-neutral-500">{description}</p>
+        <hr />
+        <Sleep/>
+        <hr />
+        <Offers/>
+        <hr />
+        <p className="text-xl font-semibold">{`Where you’ll be`}</p>
+      
+      </div>
+    </div>
+  );
+};
--- app/(dashboard)/dashboard/listings/[listingId]/_components/listingClient.tsx
+++ app/(dashboard)/dashboard/listings/[listingId]/_components/listingClient.tsx
@@ -1,40 +1,37 @@
 "use client";
 
-
-
-import { Container } from "@/components/container"
-import { ListingHead } from "./listingHead"
+import { Container } from "@/components/container";
+import { ListingHead } from "./listingHead";
 import { Listing, User } from "@prisma/client";
+import { ListingInfo } from "./lisiting-info";
 
 interface ListingClientProps {
-    listing: Listing &{
-        user: User
-    }
-    currentUser: User | null
+  listing: Listing & {
+    user: User;
+  };
+  currentUser: User | null;
 }
 
-export const ListingClient = ({
-
-    listing,
-    currentUser
-}:ListingClientProps) => {
-    
-
-    return (
-        <Container>
-            <div className="max-w-screen-lg mx-auto">
-                <div className="flex flex-col gap-6">
-                    <ListingHead
-                        title={listing.title}
-                        imageSrc={listing.imageSrc}
-                        id={listing.id}
-                        currentUser={currentUser}
-                    
-                    />
-
-                </div>
-            </div>
-        </Container>
-    )
-}
-    
+export const ListingClient = ({ listing, currentUser }: ListingClientProps) => {
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+          <div>
+            <ListingInfo
+              locationValue={listing.locationValue}
+              user={listing.user}
+              description={listing.description}
+            />
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};

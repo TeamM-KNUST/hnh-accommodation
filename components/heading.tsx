8d425c6f238e7a6685f3a64d9fc9c5d8d3e9728f
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-08 22:13:56 +0000

    Fixed heading skeleton

--- components/heading.tsx
+++ components/heading.tsx
@@ -15,17 +15,17 @@ export const Heading = ({ title, subTitle, center }: HeadingProps) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 2000);
+    }, 500);
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <div className={center ? "text-center" : "text-start"}>
       {isLoading ? (
-        <>
+        <div className="flex flex-col gap-3">
           <Skeleton className="w-[200px] h-[30px]" />
           <Skeleton className="w-[150px] h-[20px]" />
-        </>
+        </div>
       ) : (
         <>
           <div className="text-lg font-normal">{title}</div>

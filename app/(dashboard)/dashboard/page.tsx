74ff5b0efbb024da13f81d6ce5285dce4b107045
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-09 23:42:53 +0000

    Making the filterfor searching

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -6,9 +6,12 @@ import { Suspense } from "react";
 import getCurrentUser from "@/actions/getCurrentUser";
 import { EmptyState } from "@/components/empty-state";
 
+interface DashboardPageProps {
+  searchParams: IListingsParams;
+}
 
-const DashboardPage = async () => {
-  const listings = await getListings({});
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   if (listings.length === 0) {
     return (
@@ -22,11 +25,7 @@ const DashboardPage = async () => {
       <Container>
         <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
           {listings.map((listing) => (
-            <ListingCard
-              key={listing.id}
-              data={listing}
-              currentUser={currentUser}
-            />
+            <ListingCard key={listing.id} data={listing} currentUser={currentUser}/>
           ))}
         </div>
       </Container>

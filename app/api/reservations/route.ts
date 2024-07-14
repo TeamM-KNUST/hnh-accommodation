6c5a6d746f22e8aa30bfdf68ac09b3de41c639ea
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-14 03:08:05 +0000

    Making the reservation at the sever side

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,46 @@
+import getCurrentUser from "@/actions/getCurrentUser";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try {
+        const currentUser = await getCurrentUser();
+        if (!currentUser) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+        const body = await req.json();
+
+        const { listingId, startDate, endDate, totalPrice } = body;
+        
+        if (!listingId || !startDate || !endDate || !totalPrice) {
+            return new NextResponse("Bad Request", { status: 400 });
+        }
+
+        const reservation = await  db.listing.update({
+            where: {
+                id: listingId,
+            },
+            data: {
+                reservations: {
+                    create: {
+                        startDate,
+                        endDate,
+                        totalPrice,
+                        user: {
+                            connect: {
+                                id: currentUser.id,
+                            },
+                        },
+                    },
+                },
+            },
+            
+        });
+
+        return NextResponse.json(reservation);
+        
+    } catch (error) {
+        console.error(["RSERVATION_CREATE_ERROR", error]);
+        return new  NextResponse("Internal Server Error", { status: 500 });
+    }
+}

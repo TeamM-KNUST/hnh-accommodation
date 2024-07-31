2b922a249646abf78f9d3a289506355b371dc477
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-31 08:08:26 +0000

    Making the loading state for the hostel

--- data/loadingSkeleton.tsx
+++ data/loadingSkeleton.tsx
@@ -1,27 +1,122 @@
-
-import { Skeleton } from "@/components/ui/skeleton"
-
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const CategoriesSketeton = () => {
-    return (
-        <div className="grid grid-cols-3 gap-4">
-            <Skeleton className="h-20" />
-            <Skeleton className="h-20" />
-            <Skeleton className="h-20" />
-        </div>
-    )
-}
-
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      <Skeleton className="h-20" />
+      <Skeleton className="h-20" />
+      <Skeleton className="h-20" />
+    </div>
+  );
+};
 
 export const ListingCardSkeleton = () => {
   return (
-    <div className="col-span-1">
-      <Skeleton className="aspect-square w-full h-60" />
-      <div className="flex flex-col gap-1">
-        <Skeleton className="h-4 w-3/4" />
-        <Skeleton className="h-4 w-1/2" />
-        <Skeleton className="h-4 w-1/3" />
+    <>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
       </div>
-    </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>{" "}
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+      <div className="col-span-1">
+        <Skeleton className="aspect-square w-full h-60" />
+        <div className="flex flex-col gap-1">
+          <Skeleton className="h-4 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+          <Skeleton className="h-4 w-1/3" />
+        </div>
+      </div>
+    </>
   );
 };

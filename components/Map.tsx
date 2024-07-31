9508ce7ac45a6b162bccacf2a8c6df2cd3a4492a
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-31 01:17:25 +0000

    making the final side

--- components/Map.tsx
+++ components/Map.tsx
@@ -12,9 +12,11 @@ const Map = () => {
   return (
     <div
       className="h-[84vh] w-full cursor-pointer "
-      onClick={()=>router.push(
-        "https://www.google.com/maps/place/6%C2%B040'19.5%22N+1%C2%B033'39.1%22W/@6.67209,-1.560873,1751m/data=!3m1!1e3!4m4!3m3!8m2!3d6.67209!4d-1.5608728?hl=en-US&entry=ttu"
-      )}
+      onClick={() =>
+        router.push(
+          "https://www.google.com/maps/place/No+Weapon+Hostel/@6.668046,-1.5622997,1038m/data=!3m1!1e3!4m6!3m5!1s0xfdb9486ce44cd23:0xa48461f7f19a87c!8m2!3d6.6672845!4d-1.5564873!16s%2Fg%2F1v6wqplv?entry=ttu"
+        )
+      }
     >
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY! }}
